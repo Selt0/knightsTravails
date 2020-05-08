8e6fcb6ebc0666814ea47c507c57f278ad317503
Selt0 <chewedgum12@gmail.com>
2020-05-07 21:10:27 -0500

    Add method to build move tree using breadth-frist algorithm

--- knightpathfinder.rb
+++ knightpathfinder.rb
@@ -32,6 +32,19 @@ POSSIBLE_MOVES = [[ 2, -1], [ 2, 1],  # ꓶ Γ
 
   def build_move_tree
     self.root_node = PolyTreeNode.new(start_pos)
+
+    #build tree using breadth-first
+    nodes = [root_node]
+    until nodes.empty?
+      current_node = nodes.shift
+
+      current_pos = current_node.value
+      new_move_positions(current_pos).each do |next_pos|
+        next_node = PolyTreeNode.new(next_pos)
+        current_node.add_child(next_node)
+        nodes << next_node
+      end
+    end
   end
 
   def new_move_positions(pos)
